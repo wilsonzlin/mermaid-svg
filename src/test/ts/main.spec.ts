7e020938db2d030bf86e2764c3b6644379d41d22
Wilson Lin <contact@wilsonl.in>
2018-10-16 14:58:19 +1300

    Add basic test

---
+++ src/test/ts/main.spec.ts
@@ -0,0 +1,16 @@
+import chai, {expect} from "chai";
+import "mocha";
+import {generateMermaidSVG} from "../../main/ts/main";
+import chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+describe("generateMermaidSVG", () => {
+  it("should generate SVG when given valid mermaid code", () => {
+    let mermaid = `
+      graph TD
+      A-->B
+    `;
+    return expect(generateMermaidSVG(mermaid)).to.eventually.match(/^<svg /);
+  });
+});
